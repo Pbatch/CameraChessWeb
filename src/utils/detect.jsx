3f7859cc087facc655fb2d496719d193d65aea1d
peter <peter_batchelor1@hotmail.com>
2023-12-08 17:20:31 +0000

    Beginning of support for ROI

--- src/utils/detect.jsx
+++ src/utils/detect.jsx
@@ -1,11 +1,31 @@
 import * as Constants from "./constants.jsx";
 import * as tf from "@tensorflow/tfjs";
 
-export const getInput = (webcamRef) => {
-  const [input, width, height] = tf.tidy(() => {
-    const img = tf.browser.fromPixels(webcamRef.current);
-    const [height, width, _] = img.shape;
-    const imgPadded = img.pad([
+export const getInput = (webcamRef, keypoints) => {
+  let roi;
+  if (false) {
+    const padding = [50, 100, 50, 10];
+    const xs = keypoints.map(p => p[0]);
+    const ys = keypoints.map(p => p[1]);
+    roi = [
+      Math.max((Math.min(...xs) - padding[0]) / Constants.MODEL_SIZE, 0.0),
+      Math.max((Math.min(...ys) - padding[1]) / Constants.MODEL_SIZE, 0.0),
+      Math.min((Math.max(...xs) + padding[2]) / Constants.MODEL_SIZE, 1.0),
+      Math.min((Math.max(...ys) + padding[3]) / Constants.MODEL_SIZE, 1.0)
+    ]
+  } else {
+    roi = [0.0, 0.0, 1.0, 1.0]
+  }
+  const [input, originalWidth, originalHeight, width, height] = tf.tidy(() => {
+    const img = tf.browser.fromPixels(webcamRef.current)
+    const [originalHeight, originalWidth, _] = img.shape;
+    const croppedImg = img.slice(
+      [Math.round(originalHeight * roi[1]), Math.round(originalWidth * roi[0]), 0], 
+      [Math.round(originalHeight * (roi[3] - roi[1])), Math.round(originalWidth * (roi[2] - roi[0])), 
+      3]
+    );
+    const [height, width, __] = croppedImg.shape;
+    const imgPadded = croppedImg.pad([
       [0, Math.max(width, height) - height],
       [0, Math.max(width, height) - width],
       [0, 0],
@@ -14,21 +34,27 @@ export const getInput = (webcamRef) => {
       .resizeBilinear(imgPadded, [Constants.MODEL_SIZE, Constants.MODEL_SIZE])
       .div(255.0)
       .expandDims(0);
-    return [input, width, height];
+    return [input, originalWidth, originalHeight, width, height];
   });
-  return [input, width, height]
+  return [input, width, height, roi, originalWidth, originalHeight]
 };
 
 export const getBoxesAndScores = (width, height, preds) => {
+  const sx = Math.max(width, height) / width;
+  const sy = Math.max(width, height) / height;
+
   const [boxes, scores] = tf.tidy(() => {
-    const sx = Math.max(width, height) / width;
-    const sy = Math.max(width, height) / height;
     const transRes = preds.transpose([0, 2, 1]);
     const w = transRes.slice([0, 0, 2], [-1, -1, 1]).mul(sx);
     const h = transRes.slice([0, 0, 3], [-1, -1, 1]).mul(sy);
     const x = tf.sub(transRes.slice([0, 0, 0], [-1, -1, 1]).mul(sx), tf.div(w, 2));
     const y = tf.sub(transRes.slice([0, 0, 1], [-1, -1, 1]).mul(sy), tf.div(h, 2));
-    let boxes = tf.concat([x, y, tf.add(x, w), tf.add(y, h)], 2).squeeze();
+    let boxes = tf.concat([
+      x, 
+      y, 
+      tf.add(x, w), 
+      tf.add(y, h)
+    ], 2).squeeze();
     let scores = transRes.slice([0, 0, 4], [-1, -1, transRes.shape[2] - 4]).squeeze();
 
     return [boxes, scores];
