f8efd5127caf787005747eb118b660fc7f7fadc3
peter <peter_batchelor1@hotmail.com>
2023-12-08 17:24:48 +0000

    Make buttons fixed size

--- src/components/record/recordSidebar.jsx
+++ src/components/record/recordSidebar.jsx
@@ -2,12 +2,12 @@ import Display from "../common/display.jsx";
 import RecordButton from "./recordButton.jsx";
 import PgnButton from "./pgnButton.jsx";
 import HelpButton from "./helpButton.jsx";
-import CornersButton from "./cornersButton.jsx";
+import CornersButton from "../common/cornersButton.jsx";
 import HomeButton from "../common/homeButton.jsx";
 
 const RecordSidebar = ({ piecesModelRef, xcornersModelRef, webcamRef, canvasRef, sidebarRef, recording, setRecording, text, setText }) => {
   return (
-    <div ref={sidebarRef} className="d-flex flex-column text-center px-1">
+    <div ref={sidebarRef} className="d-flex flex-column text-center px-1" style={{"width": "150px"}}>
       <div className="navbar-brand text-light h1 my-2">
         ChessCam
       </div>
