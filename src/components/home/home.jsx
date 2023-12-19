f964f274f0ae8dc5fd9078a5057660484ded8491
peter <peter_batchelor1@hotmail.com>
2023-12-19 00:53:31 +0000

    Make title + logo smaller

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -7,17 +7,32 @@ import { useOutletContext } from "react-router-dom";
 const Home = () => {
   const authRef = useOutletContext().authRef;
 
+  const Title = () => {
+    return (
+      <div className="col text-end">
+        <h1>ChessCam</h1>
+      </div>
+    );
+  }
+
+  const Logo = () => {
+    return (
+      <div className="col">
+        <img 
+          src="home-128x128.svg" 
+          style={{"width": 64}} 
+          className="float-start img-fluid rounded-circle" 
+          alt="ChessCam Logo" 
+        />
+      </div>
+    )
+  }
+
   return (
     <div className="container-flex d-flex h-100 flex-column p-0 m-0 text-center text-white bg-dark">
-      <div className="row py-2 m-0">
-        <div className="col">
-          <h1>ChessCam</h1>
-        </div>
-      </div>
-      <div className="row py-2 m-0">
-        <div className="col">
-          <img src="home-128x128.svg" className="img-fluid rounded-circle" alt="ChessCam Logo" />
-        </div>
+      <div className="row py-2 m-0 align-items-center">
+        <Title />
+        <Logo />
       </div>
       <div className="row py-2 m-0">
         <div className="col">
