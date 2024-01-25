660cffd8ab5d74dc6f6e3c58715898a0b62e3fc6
Cybertone <153603746+DakshHandeCode@users.noreply.github.com>
2024-01-26 02:38:40 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,44 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from "./components/home/home";
-import Export from "./components/export/export";
-import Privacy from "./components/privacy/privacy";
-import FAQ from "./components/faq/faq";
-import App from "./App";
-
 import { createRoot } from 'react-dom/client';
-import "./style/index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from "./store.tsx";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import { registerSW } from "virtual:pwa-register";
+import store from './store.tsx';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { registerSW } from 'virtual:pwa-register';
 import { VideoAndSidebar } from './components/common';
+import App from './App';
+import Home from './components/home/home';
+import Export from './components/export/export';
+import Privacy from './components/privacy/privacy';
+import FAQ from './components/faq/faq';
+import './style/index.css';
 
 // add this to prompt for a refresh
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
+    if (window.confirm('New content available. Reload?')) {
       updateSW(true);
     }
   },
 });
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "/record",
-        element: <VideoAndSidebar mode="record" />
-      },
-      {
-        path: "/upload",
-        element: <VideoAndSidebar mode="upload" />
-      },
-      {
-        path: "/broadcast",
-        element: <VideoAndSidebar mode="broadcast" />
-      },
-      {
-        path: "/export",
-        element: <Export />
-      },
-      {
-        path: "/privacy",
-        element: <Privacy />
-      },
-      {
-        path: "/faq",
-        element: <FAQ />
-      }
-    ]
-  }
-]);
+const router = createBrowserRouter();
+
+const RootRoutes = (
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="/record" element={<VideoAndSidebar mode="record" />} />
+      <Route path="/upload" element={<VideoAndSidebar mode="upload" />} />
+      <Route path="/broadcast" element={<VideoAndSidebar mode="broadcast" />} />
+      <Route path="/export" element={<Export />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/faq" element={<FAQ />} />
+    </Route>
+  </Routes>
+);
 
 const root = createRoot(document.getElementById('root')!);
 const persistor = persistStore(store);
@@ -68,7 +46,7 @@ const persistor = persistStore(store);
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <RouterProvider router={router} />
+      <RouterProvider router={router}>{RootRoutes}</RouterProvider>
     </PersistGate>
   </Provider>
 );
