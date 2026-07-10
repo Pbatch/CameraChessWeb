613834fc2772ccf27eef0178a765bdb2ad7e9c0b
peter <peter_batchelor1@hotmail.com>
2026-07-10 20:22:58 +0100

    fix: add empty env

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": {},
+  },
   plugins: [
     react(),
     VitePWA({
